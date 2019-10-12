62daaf7ae433e857896bf206276b052ba68b4091
AntuanWSB <56278517+AntuanWSB@users.noreply.github.com>
2019-10-12 23:14:16 +0200

    Create Program sprawdzający login oraz hasło (pętla if ... else).cs

---
+++ Program sprawdzający login oraz hasło (pętla if ... else).cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Program_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nProgram sprawdzający login oraz hasło (pętla if ... else)\n\n");
+            string nazwa, haslo;
+            Console.Write("Podaj nazwę użytkownika: ");
+            nazwa = Console.ReadLine();
+            Console.Write("\nPodaj hasło użytkownika: ");
+            haslo = Console.ReadLine();
+            if (nazwa == "WSB")
+            {
+                if (haslo == "Informatyka") Console.WriteLine("\n\nZostałeś zalogowany jako " + nazwa);
+                else Console.WriteLine("\n\nBłędne hasło użytkownika");
+            }
+            else Console.WriteLine("\n\nBłędna nazwa użytkownika");
+            Console.Write("\n\nNaciśnij dowolny klawisz aby zakończyć program ... ");
+            Console.ReadKey();
+        }
+    }
+}
