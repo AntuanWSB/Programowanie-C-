0c76603b66c90ba0d807ce2c9937c1a09586b1e4
AntuanWSB <56278517+AntuanWSB@users.noreply.github.com>
2019-10-16 12:09:38 +0200

    Create Program obliczający pole prostokąta.cs

---
+++ Program obliczający pole prostokąta.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Program_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nProgram obliczający pole prostokąta\n");
+            double a, b, wynik;
+            Console.Write("\nPodaj w cm długość boku a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("\nPodaj w cm długość boku b: ");
+            b = double.Parse(Console.ReadLine());
+            wynik = a * b;
+            if (a>0 & b>0)
+            {
+                Console.WriteLine("\n\nPole prostokąta wynosi: {0} cm/kw", wynik);
+            }
+            else
+            {
+                Console.WriteLine("\n\nBłędne dane podane z klawiatury");
+            }
+            Console.Write("\n\nNaciśnij dowolny klawisz aby zakończyć program ... ");
+            Console.ReadKey();
+        }
+    }
+}
