e82446e02e58bf0954c7edceed67dd8ab9272053
AntuanWSB <56278517+AntuanWSB@users.noreply.github.com>
2019-10-16 12:11:25 +0200

    Update Program przeliczający kwotę podaną w PLN na kwotę w EUR oraz USD.cs

--- Program przeliczający kwotę podaną w PLN na kwotę w EUR oraz USD.cs
+++ Program przeliczający kwotę podaną w PLN na kwotę w EUR oraz USD.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Program_2
+namespace Program_1
 {
     class Program
     {
@@ -9,13 +9,13 @@ namespace Program_2
             Console.WriteLine("\n\nProgram przeliczający kwotę podaną w PLN na kwotę w EUR oraz USD\n\n");
             Console.Write("Podaj kwote w PLN: ");
             double kwota = Double.Parse(Console.ReadLine());
-            Console.WriteLine("\nPodałeś kwotę: " + kwota + " PLN");
+            Console.WriteLine("\nPodałeś kwotę: " + kwota + " PLN\n");
             double euro, dolar;
             euro = kwota / 4.3097;
             dolar = kwota / 3.9145;
             Console.WriteLine("{0} PLN to jest {1} EUR", kwota, Math.Round(euro, 2));
             Console.WriteLine("{0} PLN to jest {1} USD", kwota, Math.Round(dolar, 2));
-            Console.Write("\nNaciśnij dowolny klawisz aby zakończyć program ... ");
+            Console.Write("\n\nNaciśnij dowolny klawisz aby zakończyć program ... ");
             Console.ReadKey();
         }
     }
