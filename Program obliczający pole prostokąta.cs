fbd50261297cf1593fb4c2823fff5f0626f1aae1
AntuanWSB <56278517+AntuanWSB@users.noreply.github.com>
2019-10-17 16:31:40 +0200

    Update Program obliczający pole prostokąta.cs

--- Program obliczający pole prostokąta.cs
+++ Program obliczający pole prostokąta.cs
@@ -1,25 +1,33 @@
 using System;
 
-namespace Program_1
+namespace Program_3
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("\n\nProgram obliczający pole prostokąta\n");
-            double a, b, wynik;
-            Console.Write("\nPodaj w cm długość boku a: ");
-            a = double.Parse(Console.ReadLine());
-            Console.Write("\nPodaj w cm długość boku b: ");
-            b = double.Parse(Console.ReadLine());
-            wynik = a * b;
-            if (a>0 & b>0)
+            try
             {
-                Console.WriteLine("\n\nPole prostokąta wynosi: {0} cm/kw", wynik);
+                double a, b, wynik;
+                Console.Write("\nPodaj w cm długość boku a: ");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("\nPodaj w cm długość boku b: ");
+                b = double.Parse(Console.ReadLine());
+                wynik = a * b;
+                if (a > 0 & b > 0)
+                {
+                    Console.WriteLine("\n\nPole prostokąta wynosi: {0} cm/kw", wynik);
+                }
+                else
+                {
+                    Console.WriteLine("\n\nBłędne dane podane z klawiatury");
+                }
             }
-            else
+            catch (Exception ex)
             {
                 Console.WriteLine("\n\nBłędne dane podane z klawiatury");
+                //Console.WriteLine("\n\nBłędne dane podane z klawiatury {0}", ex.Message);
             }
             Console.Write("\n\nNaciśnij dowolny klawisz aby zakończyć program ... ");
             Console.ReadKey();
